b638e7cac6424fa31d5449f8598752666523bf1e
yhuangbl <yhuangbl@connect.ust.hk>
2018-09-10 14:36:07 +0800

    Change Library.java

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -4,7 +4,9 @@
 public class Library {
 	/** Add this function **/
 	public static void main(String arg[]) {
-		System.out.println("hello world");
+		Library lib = new Library();
+		if (lib.someLibraryMethod())
+			System.out.println("When there is a fire, commit and push.");
 	}
 	
     public boolean someLibraryMethod() {
